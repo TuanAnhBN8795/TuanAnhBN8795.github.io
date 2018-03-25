829cae3df0913f1db4aea9266369c8d2075c7445
DESKTOP-TGLG0JS\TuanAnh <tuananh871995@gmail.com>
2018-03-25 21:37:19 +0700

    demo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,34 +1,18 @@
 'use strict';
 var $ = jQuery;
-// navbar
-document.getElementById("close-btn").addEventListener("click", myFunction);
-document.getElementById("open-navbar").addEventListener("click", myOpenFunction);
-document.getElementById("my-navbar").style.display = "none";
-
-function myFunction() {
-    document.getElementById("my-navbar").style.display = "none";
-    document.getElementById("my-header-home").style.display = "flex";
-}
-function myOpenFunction() {
-    document.getElementById("my-navbar").style.display = "flex";
-    document.getElementById("my-header-home").style.display = "none";
-}
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
 //
 var idBrickSelected = '1-1-product1';
 var roomSelected = 'guessroom';
 var producted = 'product1';
 var row = 1;
 var col = 1;
-var room = 'guess'
+var room = 'guess';
 $(document).ready(function(){
-    var $window = $(window);
-    var windowsize = $window.width();
+    $('#my-navbar').css('display', 'none');
+    $('#myNav').css('width', '0%');
     var sizeSlide = 5;
     sizeSlide = parseInt($('#slide-history').attr('size'));
-    $("#light-slider").lightSlider({
+    $('#light-slider').lightSlider({
         gallery: true,
         item: 1,
         loop: true,
@@ -42,8 +26,33 @@ $(document).ready(function(){
         slidesToScroll: 1,
         nextArrow: '<button type="button" class="button-circle arrow-next"><i class="fa fa-angle-right"></i></button>',
         prevArrow: '<button type="button" class="button-circle arrow-prev"><i class="fa fa-angle-left"></i></button>',
+        responsive: [
+            {
+                breakpoint: 980, // tablet breakpoint
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480, // mobile breakpoint
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
     });
   });
+
+$('#close-btn').click(function() {
+    $('#my-navbar').css('display', 'none');
+    $('#my-header-home').css('display', 'flex');
+});
+
+$('#open-navbar').click(function() {
+    $('#my-navbar').css('display', 'flex');
+    $('#my-header-home').css('display', 'none');
+});
+
   $('.tech-box-prototype').click(function() {
       $('#'+idBrickSelected).removeClass('view-box-selected');
      $(this).addClass('view-box-selected');
@@ -52,36 +61,36 @@ $(document).ready(function(){
      row = parseInt(valueGet[0]);
      col = parseInt(valueGet[1]);
      producted = valueGet[2];
-     $('#imageThumb').attr("src","assets/img/"+producted+".jpg");
-     $('#imageMain').attr("src","assets/img/"+roomSelected+"/"+producted+".jpg");
+     $('#imageThumb').attr('src','assets/img/'+producted+'.jpg');
+     $('#imageMain').attr('src','assets/img/'+roomSelected+'/'+producted+'.jpg');
 });
 $('.room-type').click(function() {
     $('#'+roomSelected).removeClass('room-selected');
    $(this).addClass('room-selected');
    roomSelected = this.id;
-   $('#imageMain').attr("src","assets/img/"+roomSelected+"/"+producted+".jpg");
+   $('#imageMain').attr('src','assets/img/'+roomSelected+'/'+producted+'.jpg');
 });
 $('.button-nonfill').click(function() {
-    if(this.id=='button-select-prev'){
+    if(this.id==='button-select-prev'){
         col = col - 1;
-        if(col == 0){
+        if(col === 0){
             row = row - 1;
-            if(row == 0){
+            if(row === 0){
                 row = 6;
             }
             col = 6;
         }
-    }else if(this.id=='button-select-next'){
+    }else if(this.id==='button-select-next'){
         col = col + 1;
-        if(col == 7){
+        if(col === 7){
             row = row + 1;
-            if(row == 7){
+            if(row === 7){
                 row = 1;
             }
             col = 1;
         }
     }
-    var arrayResult = $('[id^='+row+"-"+col+']');
+    var arrayResult = $('[id^='+row+'-'+col+']');
     $('#'+idBrickSelected).removeClass('view-box-selected');
      $(arrayResult[0]).addClass('view-box-selected');
      idBrickSelected = arrayResult[0].id;
@@ -89,8 +98,8 @@ $('.button-nonfill').click(function() {
      row = parseInt(valueGet[0]);
      col = parseInt(valueGet[1]);
      producted = valueGet[2];
-     $('#imageThumb').attr("src","assets/img/"+producted+".jpg");
-     $('#imageMain').attr("src","assets/img/"+roomSelected+"/"+producted+".jpg");
+     $('#imageThumb').attr('src','assets/img/'+producted+'.jpg');
+     $('#imageMain').attr('src','assets/img/'+roomSelected+'/'+producted+'.jpg');
 });
 
 
